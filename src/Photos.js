aedef09aaa5e8a27740a006dff107de6952d6a6b
Shazammon <ammonorgill@gmail.com>
2023-02-03 16:20:19 -0300

    wrote handleclick function

--- src/Photos.js
+++ src/Photos.js
@@ -3,6 +3,10 @@ import React, { useState } from 'react'
 export default function Photos() {
     const [photos, setPhotos] = useState()
 
+    function handleClick() {
+        
+    }
+
     return (
         <>
 
