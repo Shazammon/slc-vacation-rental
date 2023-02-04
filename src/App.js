531b589ca7d80d8e80b940f2b9fd191f4b748481
Shazammon <ammonorgill@gmail.com>
2023-02-04 13:16:16 -0300

    set state for a test variable

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import React, { useState, useRef } from 'react';
 function App() {
   const ref = useRef(0)
 
+  const [ test, setTest ] = useState()
+  setTest("hi")
+
   return (
     <>
-      <Photos />
+      <Photos test={test} />
     </>
   );
 }
