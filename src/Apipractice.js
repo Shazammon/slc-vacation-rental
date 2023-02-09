e234bc7a9d3f8109c340aad9b86f6f54d2f61861
Shazammon <ammonorgill@gmail.com>
2023-02-09 09:51:37 -0700

    more api get practice with fetch

--- src/Apipractice.js
+++ src/Apipractice.js
@@ -7,3 +7,8 @@ import fetch from "node-fetch"
 //     .then(data => {
 //         setBlogs(data)
 //         console.log(data)})
+
+fetch('https://reqres.in/api/users')
+    .then(res => {
+        console.log(res)
+    })
